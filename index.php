3c20bea7a5dde96a3c3033ea2fdc157c2c620c20
Arina Iljinih <iljinih.ar@yandex.ru>
2022-06-19 14:25:46 +0700

    69.2

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 	require_once 'Link.php';
-	// Выведет <a href="">index</a>:
-	echo (new Link())->setText('index')->show();
+	
+	echo (new Link())->setAttr('href', '1.php')->setText('1')->show(). '<br>';
+	echo (new Link())->setAttr('href', '2.php')->setText('2')->show(). '<br>';
+	echo (new Link())->setAttr('href', '3.php')->setText('3')->show(). '<br>';
+	echo (new Link())->setAttr('href', '4.php')->setText('4')->show(). '<br>';
+	echo (new Link())->setAttr('href', '5.php')->setText('5')->show(). '<br>';
+
 ?>
