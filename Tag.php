945296d5a19a0e004e5f4c6c1266d026d61e156d
Arina Iljinih <iljinih.ar@yandex.ru>
2022-06-19 11:52:39 +0700

    61.1, 61.2

--- Tag.php
+++ Tag.php
@@ -2,39 +2,52 @@
 	class Tag
 	{
 		private $name;
-        private $attrs;
+		private $attrs = []; // изначально пустой массив
+        private $rem; 
 		
 		public function __construct($name)
 		{
 			$this->name = $name;
 		}
 		
-		// Выводим открывающую часть тега:
+		// Реализуем метод для атрибутов:
+		public function setAttr($name, $value)
+		{
+			$this->attrs[$name] = $value;
+			return $this; // возвращаем $this чтобы была цепочка
+		}
+		
 		public function open()
 		{
 			$name = $this->name;
-			return "<$name>";
+			$attrsStr = $this->getAttrsStr($this->attrs);
+			
+			return "<$name$attrsStr>";
 		}
-        // Выводим закрывающую часть тега:
+		
 		public function close()
 		{
 			$name = $this->name;
 			return "</$name>";
 		}
-
-        private function getAttrsStr($attrs)
-        {
-            if (!empty($attrs)) {
-                $result = '';
-                
-                foreach ($attrs as $name => $value) {
-                    $result .= " $name=\"$value\"";
-                }
-                
-                return $result;
-            } else {
-                return '';
-            }
+		
+		private function getAttrsStr($attrs)
+		{
+			if (!empty($attrs)) {
+				$result = '';
+				
+				foreach ($attrs as $name => $value) {
+					$result .= " $name=\"$value\"";
+				}
+				
+				return $result;
+			} else {
+				return '';
+			}
+		}
+        private function removeAttr($rem){
+            unset($this->attrs[$rem]);
+			return $this;	
         }
 	}
 ?>
