b3f56f0b81af3fbf8927e6ce60460a19060436ea
Arina Iljinih <iljinih.ar@yandex.ru>
2022-06-19 12:16:14 +0700

    67

--- Tag.php
+++ Tag.php
@@ -1,20 +1,43 @@
 <?php
-	class Tag
+	require_once 'iTag.php';
+	class Tag implements iTag
 	{
 		private $name;
-		private $attrs = []; 
-        private $rem; 
+		private $attrs = [];
+		private $text = '';
 		
 		public function __construct($name)
 		{
 			$this->name = $name;
 		}
 		
-		
-		public function setAttr($name, $value)
+		public function getName()
 		{
-			$this->attrs[$name] = $value;
-			return $this; 
+			return $this->name;
+		}
+		
+		public function getText()
+		{
+			return $this->text;
+		}
+		
+		public function getAttrs()
+		{
+			return $this->attrs;
+		}
+		
+		public function getAttr($name)
+		{
+			if (isset($this->attrs[$name])) {
+				return $this->attrs[$name];
+			} else {
+				return null;
+			}
+		}
+		
+		public function show()
+		{
+			return $this->open() . $this->text . $this->close();
 		}
 		
 		public function open()
@@ -31,17 +54,73 @@
 			return "</$name>";
 		}
 		
+		public function setText($text)
+		{
+			$this->text = $text;
+			return $this;
+		}
+		
+		public function setAttr($name, $value = true)
+		{
+			$this->attrs[$name] = $value;
+			return $this;
+		}
+		
+		public function setAttrs($attrs)
+		{
+			foreach ($attrs as $name => $value) {
+				$this->setAttr($name, $value);
+			}
+			
+			return $this;
+		}
+		
+		public function removeAttr($name)
+		{
+			unset($this->attrs[$name]);
+			return $this;
+		}
+		
+		public function addClass($className)
+		{
+			if (isset($this->attrs['class'])) {
+				$classNames = explode(' ', $this->attrs['class']);
+				
+				if (!in_array($className, $classNames)) {
+					$classNames[] = $className;
+					$this->attrs['class'] = implode(' ', $classNames);
+				}
+			} else {
+				$this->attrs['class'] = $className;
+			}
+			
+			return $this;
+		}
+		
+		public function removeClass($className)
+		{
+			if (isset($this->attrs['class'])) {
+				$classNames = explode(' ', $this->attrs['class']);
+				
+				if (in_array($className, $classNames)) {
+					$classNames = $this->removeElem($className, $classNames);
+					$this->attrs['class'] = implode(' ', $classNames);
+				}
+			}
+			
+			return $this;
+		}
+		
 		private function getAttrsStr($attrs)
 		{
 			if (!empty($attrs)) {
 				$result = '';
 				
 				foreach ($attrs as $name => $value) {
-					
 					if ($value === true) {
-						$result .= " $name"; 
+						$result .= " $name";
 					} else {
-						$result .= " $name=\"$value\""; 
+						$result .= " $name=\"$value\"";
 					}
 				}
 				
@@ -50,71 +129,13 @@
 				return '';
 			}
 		}
-        private function removeAttr($rem){
-            unset($this->attrs[$rem]);
-			return $this;	
-        }
-
-		public function setAttrs($attrs){
-		foreach ($attrs as $name => $value) {
-			$this->setAttr($name, $value);
-		}
-		return $this;
-	}
-
-	public function addClass($className)
-	{
-		if (isset($this->attrs['class'])) {
-			$classNames = explode(' ', $this->attrs['class']);
+		
+		private function removeElem($elem, $arr)
+		{
+			$key = array_search($elem, $arr);
+			array_splice($arr, $key, 1);
 			
-			if (!in_array($className, $classNames)) {
-				
-				$classNames[] = $className;
-				
-				$this->attrs['class'] = implode(' ', $classNames);
-			}
+			return $arr;
 		}
-		
-		return $this;
-	}
-
-	private function removeElem($elem, $arr)
-	{
-		$key = array_search($elem, $arr); 
-		array_splice($arr, $key, 1); 
-		
-		return $arr; 
-	}
-
-	public function removeClass($className)
-	{
-		if (isset($this->attrs['class'])) {
-			$classNames = explode(' ', $this->attrs['class']);
-			
-			if (in_array($className, $classNames)) {
-				$classNames = $this->removeElem($className, $classNames);
-				$this->attrs['class'] = implode(' ', $classNames);
-			}
-		}
-		
-		return $this;
-	}
-
-	public function getName(){
-		return $this->name;
-	}
-	public function getText(){
-		return $this->text;
-	}
-	public function getAttrs(){
-		return $this->attrs;
-	}
-	public function getAttr($attr){
-		if (isset($this->attrs[$attr])){
-		return $this->attrs[$attr];
-	} else {
-		return null;
-	}
-	}
 	}
 ?>
